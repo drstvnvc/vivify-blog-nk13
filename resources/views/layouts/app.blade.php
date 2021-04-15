d829bd6e2a87d5ca2f3a4db61e51e648b209685e
Drago Stevanovic <drago.s@vivifyideas.com>
2021-04-15 21:55:10 +0200

    Add navbar blade template and include it in layouts.app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,13 +1,12 @@
 <html>
   <head>
     <title>@yield('title')</title>
-    <style>
-      body {
-        font-family: 'Nunito', sans-serif;
-      }
-    </style>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
   </head>
   <body>
-    @yield('content')
+    @include('navbar')
+    <div class="container">
+      @yield('content')
+    </div>
   </body>
 </html>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,8 +3,6 @@
 @section('title', $post->title)
 
 @section('content')
-<a href="/posts">Home</a>
-    
 <h2>{{$post->title}}</h2>
 <p>{{$post->body}}</p>
 @endsection
